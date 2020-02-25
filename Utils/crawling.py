947bd061773db72b0103ffd0fc9960615fffe3b6
songyeoneo <dbsgh3344@naver.com>
2020-02-25 13:46:14 +0900

    Add JTBC News Title & Content Crawling data

--- Utils/crawling.py
+++ Utils/crawling.py
@@ -1,4 +1,3 @@
-#-*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -6,17 +5,17 @@ import re
 import time
 import signal
 import sys
+from datetime import datetime,timedelta
 
 answer_list = []
 question_list = []
 
 def jobkorea_crawling(start_page,end_page,url,path,qus_tag='dl.qnaLists span.tx',ans_tag='dl.qnaLists dd div.tx'): 
-    # 1 page ë‹¹ 20ê±´ì˜ ìì†Œì„œê°€ ì¡´ì¬ 
-    # url = jobkorea url ì…ë ¥
-    # start_page = ì‹œì‘ í˜ì´ì§€
-    # end_page = ì¢…ë£Œ í˜ì´ì§€
-    global path_ 
-    path_= path
+    # 1 page ´ç 20°ÇÀÇ ÀÚ¼Ò¼­°¡ Á¸Àç 
+    # url = jobkorea url ÀÔ·Â
+    # start_page = ½ÃÀÛ ÆäÀÌÁö
+    # end_page = Á¾·á ÆäÀÌÁö
+    
     url_lists = []
 
     for i in range(start_page,end_page):
@@ -30,22 +29,22 @@ def jobkorea_crawling(start_page,end_page,url,path,qus_tag='dl.qnaLists span.tx'
         for j in a:
             url_lists.append('http://www.jobkorea.co.kr'+j.find('a').attrs['href'])
 
-    return qus_ans(url_lists,qus_tag,ans_tag,path_)
+    return qus_ans(url_lists,qus_tag,ans_tag,path)
 
 
 
 def signal_handler(signal,frame):
-    #ì¤‘ê°„ì— ì¤‘ì§€ëª…ë ¹ì„ ë‚´ë¦´ ì‹œ, ì‘ì—…ì¤‘ì´ë˜ ë°ì´í„°ë¥¼ ì €ì¥
+    #Áß°£¿¡ ÁßÁö¸í·ÉÀ» ³»¸± ½Ã, ÀÛ¾÷ÁßÀÌ´ø µ¥ÀÌÅÍ¸¦ ÀúÀå
     df = pd.DataFrame(question_list,columns=['qus'])
     df['ans'] = answer_list     
-    df.to_csv(path_)
-    print('Last operation before end of the program', '*'*100)
+    df.to_csv(path)
+    print('jobkorea qus&ans data crawling stop and save about data completed until now', '*'*100)
     sys.exit(0)
 
 
 def qus_ans(url_lists,qus_tag,ans_tag,path):
-    # í¬ë¡¤ë§í•œ ë°ì´í„°ë¥¼ ì§ˆë¬¸ê³¼ ë‹µë³€ìœ¼ë¡œ ë‚˜ëˆ ì„œ csvë¡œ ì €ì¥
-    pat = re.compile('<span[\sa-zA-Z0-9="]*>[ê°€-í£\sa-zA-Z0-9<>\/,]+<\/span>')
+    # Å©·Ñ¸µÇÑ µ¥ÀÌÅÍ¸¦ Áú¹®°ú ´äº¯À¸·Î ³ª´²¼­ csv·Î ÀúÀå
+    pat = re.compile('<span[\sa-zA-Z0-9="]*>[°¡-ÆR\sa-zA-Z0-9<>\/,]+<\/span>')
     parser = 'html.parser'
     signal.signal(signal.SIGINT, signal_handler)
 
@@ -65,7 +64,116 @@ def qus_ans(url_lists,qus_tag,ans_tag,path):
  
     df = pd.DataFrame(question_list,columns=['qus'])
     df['ans'] = answer_list     
-    df.to_csv(path_)
+    df.to_csv(path)
 
     return  df
 
+
+
+def date_split(x):
+    y,m,d = x.split(',')
+    return int(y),int(m),int(d)
+
+
+
+def url_func(selector):
+    for j in selector:
+        news_url = 'http://news.jtbc.joins.com/' + j.attrs['href']
+        # print(news_url)
+        yield news_url
+
+def jtbc_news(date_start,date_end,path):
+    # date_start : ½ÃÀÛ ³¯Â¥. ÃÖ½Å ³¯Â¥ºÎÅÍ ½ÃÀÛ
+    # date_end : ³¡³ª´Â ³¯Â¥. °¡Àå ³ªÁß ³¯Â¥
+    # scode: ´º½º ±â»ç Ä«Å×°í¸® ex) ³¯¾¾,°æÁ¦ µî
+    global path2
+    path2 = path
+
+    url = 'http://news.jtbc.joins.com/section/list.aspx'
+    url_lists = []
+    y_s,m_s,d_s = date_split(date_start)
+    y_e,m_e,d_e = date_split(date_end)   
+    date_start = datetime(y_s,m_s,d_s)
+    date_end = datetime(y_e,m_e,d_e) 
+    date_diff = (date_start - date_end).days
+
+    section_url={}
+    
+
+    for i in range(date_diff,-1,-1):
+        for code in range(0,10,10):
+            datestr = (date_start-timedelta(i)).strftime('%Y%m%d')
+            params = {'pdate':datestr,'scode':code}
+            res = requests.get(url,params= params)
+            soup = BeautifulSoup(res.text,'html.parser')
+            selector = soup.select('ul#section_list dt.title_cr a')
+            
+
+            next_page= soup.select('div#CPContent_pager a.next')
+            
+            url_lists += [i for i in url_func(selector)]
+
+            while len(next_page) >0:
+                next_url = 'http://news.jtbc.joins.com/' + next_page[0].attrs['href']
+                res = requests.get(next_url)
+                soup = BeautifulSoup(res.text,'html.parser')
+                selector = soup.select('ul#section_list dt.title_cr a')
+                
+                url_lists+= [i for i in url_func(selector)]
+                
+                next_page = soup.select('div#CPContent_pager a.next')
+                
+            section_url[code] = url_lists
+            
+
+    return title_text_split(section_url)
+
+
+def jtbc_signal_handler(signal,frame):
+    
+    df = pd.DataFrame(code_list,columns=['section'])
+    df['title'] = title_list     
+    df['content'] = content_list
+    df.to_csv(path2)
+    print('jtbc_news_crawling stop and save about data completed until now ', '*'*100)
+    sys.exit(0)
+
+
+def title_text_split(section_url):
+    signal.signal(signal.SIGINT, jtbc_signal_handler)
+
+    section_mapping = {0:'¼Óº¸',
+                       10:'Á¤Ä¡',
+                       20:'°æÁ¦',
+                       30:'»çÈ¸',
+                       40:'±¹Á¦',
+                       50:'¹®È­',
+                       60:'¿¬¿¹',
+                       70:'½ºÆ÷Ã÷',
+                       80:'³¯¾¾'
+    }
+
+    global title_list
+    title_list = []
+    global content_list
+    content_list =[]
+    global code_list
+    code_list = []
+    for code,urls in section_url.items():
+        for url in urls:
+            res = requests.get(url)
+            soup = BeautifulSoup(res.content,'html.parser')
+
+            title = soup.select_one('div.title h3').text
+            content = soup.select_one('div.article_content').text
+
+            title_list.append(title)
+            content_list.append(content)
+            code_list.append(section_mapping[code])      
+            
+    df = pd.DataFrame(code_list,columns=['section'])
+    df['title'] = title_list
+    df['content'] = content_list
+    df.to_csv(path2)
+
+    return df
